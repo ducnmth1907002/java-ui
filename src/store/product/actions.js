6f92a1cb28e76c73e01f1543350cc26d92cac857
Your Name <ducnmth1907002@fpt.edu.vn>
2021-06-21 21:17:51 +0700

    update

--- src/store/product/actions.js
+++ src/store/product/actions.js
@@ -1,8 +1,9 @@
 export async function loadProducts({commit}) {
     commit('fetchProductsBegin')
-    fetch('http://127.0.0.1:8089/products')
+    fetch('http://127.0.0.1:8089/product')
         .then(response => response.json())
         .then((data) => {
+            console.log(data)
             commit('fetchProductsSuccess', {
                 data: data
             })

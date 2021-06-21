9153dec78b6d2f890a357d9b62ebc1bef3b3108c
Your Name <ducnmth1907002@fpt.edu.vn>
2021-06-21 21:07:37 +0700

    view

---
+++ src/store/product/actions.js
@@ -0,0 +1,10 @@
+export async function loadProducts({commit}) {
+    commit('fetchProductsBegin')
+    fetch('http://127.0.0.1:8089/products')
+        .then(response => response.json())
+        .then((data) => {
+            commit('fetchProductsSuccess', {
+                data: data
+            })
+        })
+}
